2950b19d449e8e3620e79bf0ab8025fdc527e095
dollykumari9798 <rppandey9798@gmail.com>
2022-07-16 14:41:19 +0530

    chass_base_updated

---
+++ lib/box.dart
@@ -0,0 +1,528 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: Myapp()));
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: GridView.count(crossAxisCount: 8, children: [
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.grey,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(1),
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.black,
+        ),
+      ),
+    ]));
+  }
+}
